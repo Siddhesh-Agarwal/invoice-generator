8cb4b280fc74059c7cba63f5f219bda60802c5a7
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2025-08-27 18:06:28 +0530

    fixed zod schema

--- src/types/invoice.ts
+++ src/types/invoice.ts
@@ -11,8 +11,8 @@ export type LineItemType = z.infer<typeof lineItemSchema>;
 export const businessSchema = z.object({
   name: z.string().min(1, {message: "Name is required"}),
   email: z.email({message: "Invalid email address"}).optional(),
-  address: z.string().min(1, {message: "Address is invalid"}).optional(),
-  phone: z.string().min(1, {message: "Phone number is invalid"}).optional(),
+  address: z.string().optional(),
+  phone: z.string().optional(),
   logoUrl: z.base64url({message: "Invalid logo URL"}).optional(),
 });
 
@@ -21,8 +21,8 @@ export type BusinessDetailsType = z.infer<typeof businessSchema>;
 export const clientSchema = z.object({
   name: z.string().min(1, {message: "Name is required"}),
   email: z.email({message: "Invalid email address"}).optional(),
-  address: z.string().min(1, {message: "Address is invalid"}).optional(),
-  phone: z.string().min(1, {message: "Phone number is invalid"}).optional(),
+  address: z.string().optional(),
+  phone: z.string().optional(),
   logoUrl: z.base64url({message: "Invalid logo URL"}).optional(),
 });
 
@@ -40,17 +40,24 @@ export const invoiceSchema = z.object({
   invoiceDetails: invoiceDetailsSchema,
   businessDetails: businessSchema,
   clientDetails: clientSchema,
-  lineItems: z.array(lineItemSchema),
+  lineItems: z.array(lineItemSchema).default([]),
   notes: z.string().optional(),
-  subtotal: z.number().min(0, {message: "Subtotal must be greater than 0"}),
-  taxRate: z.number().min(0, {message: "Tax rate must be greater than 0"}),
-  taxAmount: z.number().min(0, {message: "Tax amount must be greater than 0"}),
-  total: z.number().min(0, {message: "Total must be greater than 0"}),
+  subtotal: z
+    .number()
+    .min(0, {message: "Subtotal must be greater than 0"})
+    .default(0),
+  taxRate: z
+    .number()
+    .min(0, {message: "Tax rate must be greater than 0"})
+    .default(0),
+  taxAmount: z
+    .number()
+    .min(0, {message: "Tax amount must be greater than 0"})
+    .default(0),
+  total: z
+    .number()
+    .min(0, {message: "Total must be greater than 0"})
+    .default(0),
 });
 
 export type InvoiceType = z.infer<typeof invoiceSchema>;
-
-export type InvoiceTypePreview = Pick<
-  InvoiceType,
-  "invoiceDetails" | "clientDetails" | "total"
->;
